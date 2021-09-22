35b5b319e5fc1a5118c1ef9831a1c811a91aa7cd
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2021-09-22 22:40:04 +0900

    Add failing test

--- src/test/UtilTest.ts
+++ src/test/UtilTest.ts
@@ -2,7 +2,7 @@ import test from "ava";
 import * as dayjs from "dayjs";
 import { Schedule } from "../services/Spla2API";
 
-import { isTargetRule } from "../Util";
+import { isTargetRule, stageRange } from "../Util";
 
 test("isTargetRule returns true when current time is between start and end", (t) => {
   const s: Schedule = {
@@ -40,4 +40,11 @@ test("isTargetRule returns false when current time is out of start and end", (t)
   };
   const actual = isTargetRule(s, dayjs("2020-12-29T23:00:00"));
   t.false(actual);
+});
+
+test("stageRange returns 23 hour on 0 o'clock", (t) => {
+  const d = dayjs("2021-09-22T00:00:00");
+  const [start, end] = stageRange(d);
+  t.is(start, 23);
+  t.is(end, 1);
 });
