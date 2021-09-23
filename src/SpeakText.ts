06ee9bb54a95e212516f3fac598770a784020f43
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2021-09-23 13:33:26 +0900

    Use template

--- src/SpeakText.ts
+++ src/SpeakText.ts
@@ -1,7 +1,7 @@
 import * as dayjs from "dayjs";
+import * as mustache from "mustache";
 import { Schedule, ShakeSchedule } from "./services/Spla2API";
 import { stageRange } from "./Util";
-import * as mustache from "mustache";
 
 export interface AlexaResponse {
   speakText: string;
@@ -51,14 +51,14 @@ export function shakeText(
 }
 
 const gachiAndLeagueTextTemplate: string = `{{#alreadyStarted}}
-{{end}}時まで
+{{begin}}時から{{end}}時まで
 {{/alreadyStarted}}
 {{^alreadyStarted}}
-{{begin}}時から{{end}}時まで
+{{end}}時まで
 {{/alreadyStarted}}
 のガチマは{{gachiRule}}で、ステージは{{gachiMap1}}と{{gachiMap2}}だよ。
 リグマは{{leagueRule}}で、ステージは{{leagueMap1}}と{{leagueMap2}}だよ。
-`;
+`.replace(/(\r\n|\n|\r)/gm, "");
 
 export function gachiAndLeagueText(
   currentTime: dayjs.Dayjs,
@@ -66,18 +66,23 @@ export function gachiAndLeagueText(
   league: Schedule,
   now: dayjs.Dayjs = dayjs()
 ): AlexaResponse {
-  const gachiMap = gachi.maps.join("と");
-  const leagueMap = league.maps.join("と");
   const [begin, end] = stageRange(currentTime);
-  const start =
-    now < currentTime ? `${begin}時から${end}時まで` : `${end}時まで`;
-  const text = `${start}のガチマは${gachi.rule}で、ステージは${gachiMap}だよ。リグマは${league.rule}で、ステージは${leagueMap}だよ。`;
   const images = gachi.maps_ex
     .map((m) => m.image)
     .concat(league.maps_ex.map((m) => m.image));
-  const image = images[Math.floor(Math.random() * images.length)];
+  const params = {
+    alreadyStarted: now < currentTime,
+    begin: begin,
+    end: end,
+    gachiRule: gachi.rule,
+    gachiMap1: gachi.maps[0],
+    gachiMap2: gachi.maps[1],
+    leagueRule: league.rule,
+    leagueMap1: league.maps[0],
+    leagueMap2: league.maps[1],
+  };
   return {
-    speakText: text,
+    speakText: mustache.render(gachiAndLeagueTextTemplate, params),
     cardTitle: `${begin}時 ~ ${end}時のガチマとリグマ`,
     cardText: [
       `${gachi.rule}`,
@@ -85,6 +90,6 @@ export function gachiAndLeagueText(
       `${league.rule}`,
       league.maps.map((x) => `- ${x}`).join("\n"),
     ].join("\n"),
-    cardImage: image,
+    cardImage: images[Math.floor(Math.random() * images.length)],
   };
 }
