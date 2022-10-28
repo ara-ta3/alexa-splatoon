82f093fbbf071549cbf307ccbe4ac381a8637e9d
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2022-10-28 23:07:33 +0900

    Remove splatoon2 temporary

--- src/index.ts
+++ src/index.ts
@@ -9,26 +9,16 @@ import { handlers } from "./handlers";
 import { Splatoon3InkAPIClientImpl } from "./services/Splatoon3InkAPI";
 
 let skill: CustomSkill = null;
-let splatoon2: AlexaSplatoon2 | null = null;
 let splatoon3: AlexaSplatoon3 | null = null;
 
 const handler = async (event: RequestEnvelope, context: any, _: Function) => {
-  if (splatoon2 === null) {
-    splatoon2 = new AlexaSplatoon2(
-      new Spla2APIClientImpl(process.env.USER_AGENT)
-    );
-  }
   if (splatoon3 === null) {
     splatoon3 = new AlexaSplatoon3(new Splatoon3InkAPIClientImpl());
   }
 
-  if (splatoon2.empty()) {
-    await splatoon2.update();
-  }
-
   if (skill === null) {
     skill = SkillBuilders.custom()
-      .addRequestHandlers(...handlers(splatoon2, splatoon3))
+      .addRequestHandlers(...handlers(splatoon3))
       .create();
   }
 
